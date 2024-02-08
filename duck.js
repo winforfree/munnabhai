6a09a89644855374bd5b39890b41bd308db446bf
winforfree <147972746+winforfree@users.noreply.github.com>
2024-02-08 19:27:47 +0600

    Delete duck.js

--- duck.js
+++
@@ -1,4 +0,0 @@
-var logoText = "Rokeya";
-var siteName = "Rokeya";
-var bestLink = "https://www.toprevenuegate.com/bzae0vqwhv?key=e118e256b319cbc8165df28a34a4ed40";
-document.title = "Click Here To Watch";
